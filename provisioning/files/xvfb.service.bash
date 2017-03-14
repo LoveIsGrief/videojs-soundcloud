879833cf59ca79c7c44d1546d33ccff0988c60a0
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2017-03-14 21:36:19 +0100

    Let the xvfb service post its errors
    LoveIsGrief/videojs-soundcloud#23 Vagrantfile

--- provisioning/files/xvfb.service.bash
+++ provisioning/files/xvfb.service.bash
@@ -4,21 +4,19 @@ PIDFILE=/tmp/cucumber_xvfb_99.pid
 case "$1" in
   start)
     echo -n "Starting virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
-    echo "."
+    /sbin/start-stop-daemon --start --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
     ;;
   stop)
     echo -n "Stopping virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
+    /sbin/start-stop-daemon --stop --pidfile $PIDFILE
     rm -f $PIDFILE
-    echo "."
     ;;
   restart)
     $0 stop
     $0 start
     ;;
   *)
-  echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
-  exit 1
+    echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
+    exit 1
+    ;;
 esac
-exit 0
